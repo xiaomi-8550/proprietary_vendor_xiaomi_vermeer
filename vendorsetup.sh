044bf2533ec996bd36e72bb5dc606c065878b1dc
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-10-27 14:49:30 +0100

    vermeer: Switch to python extract-utils
    Change-Id: Ifd7f36726c7fe6a3ebcc96de07125b7e08ec1631

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,8 @@
-# Lets assemble back blobs from its split parts
-cat vendor/xiaomi/vermeer/proprietary/odm/etc/camera/aivsModel_6C06C006.part* > vendor/xiaomi/vermeer/proprietary/odm/etc/camera/aivsModel_6C06C006
-cat vendor/xiaomi/vermeer/proprietary/odm/lib64/libmialgo_aisn.so.part* > vendor/xiaomi/vermeer/proprietary/odm/lib64/libmialgo_aisn.so
+#!/bin/bash
+# Script to dynamically reassemble .part* files
+
+for file in $(find "vendor/xiaomi/vermeer" -type f -name "*.part00"); do
+    base_name=${file%.part00}
+    cat ${base_name}.part* > $base_name
+    echo "Reassembly of $base_name complete!"
+done
